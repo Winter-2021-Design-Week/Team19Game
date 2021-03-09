ec988b1b4e76e397ddb9697966a75bb480aff394
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2021-03-09 15:39:57 -0500

    Add basic tilemap example scene w/ layering and collision

---
+++ ShoppingGame/Assets/Scripts/LerpTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpTarget : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+
+    public float speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * speed);
+        }
+    }
+}
