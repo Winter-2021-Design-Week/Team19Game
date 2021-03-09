97029e1acc4e8e7c2e765d17ef4053beb5e236e4
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2021-03-09 14:17:40 -0500

    Add basic character controller and animator

---
+++ ShoppingGame/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+
+    [SerializeField]
+    float speed = 3f, currentSpeed;
+    Vector2 stepDirection;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            stepDirection.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            stepDirection.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            stepDirection.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            stepDirection.x = 1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        stepDirection.Normalize();
+
+        rb.MovePosition((Vector2)transform.position + stepDirection * speed * Time.deltaTime);
+
+        currentSpeed = rb.velocity.magnitude;
+
+        // Temporary animation parameter output
+        anim.SetFloat("currentSpeed", stepDirection.magnitude);
+
+        stepDirection = Vector2.zero;
+    }
+}
