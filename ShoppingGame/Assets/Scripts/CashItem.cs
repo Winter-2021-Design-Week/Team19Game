58ecb96e1b2a0bc1aacc52ec346a72a42c7a28a1
Adam-E-B <61750702+Adam-E-B@users.noreply.github.com>
2021-03-09 18:29:06 -0500

    Add basic game loop w/ timer, pickups, and final scores

---
+++ ShoppingGame/Assets/Scripts/CashItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CashItem : MonoBehaviour
+{
+    // Cash value of product
+    [SerializeField]
+    int value;
+
+    [SerializeField]
+    Text valueText;
+
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+        valueText.text = "$" + value;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            gm.AddCash(value);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ ShoppingGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{   
+    public bool levelOver;
+
+    // Timer and score variables
+    public Text timer, finalReadout;
+
+    public int currentCash, maxCash, maxTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalReadout.gameObject.SetActive(false);
+
+        levelOver = false;
+
+        currentCash = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Restart scene at time
+        if (Time.timeSinceLevelLoad >= maxTime && levelOver == false)
+        {
+            levelOver = true;
+            ReadScore();
+        }
+        else if (Time.timeSinceLevelLoad <= maxTime)
+        {
+            // Timer's visual update
+            timer.text = (maxTime - Time.timeSinceLevelLoad).ToString("00:00");
+        }
+    }
+
+    public void AddCash(int amount)
+    {
+        currentCash += amount;
+    }
+
+    void ReadScore()
+    {
+        // Show final score
+        finalReadout.gameObject.SetActive(true);
+        finalReadout.text = "You spent:\n$" + currentCash + "/$" + maxCash + "\nThat's\n" + (((float)currentCash / maxCash) * 100).ToString("00.00") + "%\nof your budget!";
+
+        // If over budget
+        if ((float)currentCash / maxCash > 1)
+        {
+            finalReadout.text += "\nToo bad! You're over budget.";
+        }
+        else if ((float)currentCash / maxCash == 1)
+        {
+            finalReadout.text += "\nNice! You're right on budget.";
+        }
+        else if ((float)currentCash / maxCash > .5f && (float)currentCash / maxCash < 1)
+        {
+            finalReadout.text += "\nPretty good! You're close to budget.";
+        }
+        else
+        {
+            finalReadout.text += "\nNo good. You're way under budget.";
+        }
+
+        // End level in 5 seconds
+        Invoke("EndLevel", 5);
+    }
+
+    void EndLevel()
+    {
+        // Just to be safe, cancel all invokes
+        CancelInvoke();
+
+        // Temporary level call
+        SceneManager.LoadScene("TestLevel");
+    }
+}
